90b06c57dfd559364159f9f300360982370183b3
CSQ <eRicCSQ0611@outlook.com>
2022-04-24 23:23:41 +0800

    206反转链表

---
+++ 206_reserveList.c
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    while(curr){
+
+        struct ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+
+}
+
+/*
+206. 反转链表
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+
+示例 1：
+
+
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+示例 2：
+
+
+输入：head = [1,2]
+输出：[2,1]
+示例 3：
+
+输入：head = []
+输出：[]
+
+
+提示：
+
+链表中节点的数目范围是 [0, 5000]
+-5000 <= Node.val <= 5000
+*/
